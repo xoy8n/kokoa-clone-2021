a95fff0e1835f38aecc0920a2d65fb7555d30a85
xoy8n <s01057111917@gmail.com>
2021-10-13 10:24:03 +0900

    #2.0 redirect

---
+++ js/blackmode.js
@@ -0,0 +1,32 @@
+"use strick";
+let body = document.body;
+let darkModeBtn = document.querySelector(".mode");
+let darkMode = localStorage.getItem("darkMode");
+
+const DARK_MODE = "darkMode";
+
+const enableDarkMode = () => {
+  darkModeBtn.innerHTML = `<i class="fas fa-toggle-on"></i>`;
+  document.body.classList.add("dark-theme");
+  localStorage.setItem(DARK_MODE, "enabled");
+};
+
+const disableDarkMode = () => {
+  darkModeBtn.innerHTML = `<i class="fas fa-toggle-off"></i>`;
+  document.body.classList.remove("dark-theme");
+  localStorage.setItem(DARK_MODE, null);
+};
+
+function darkModeLoad() {}
+if (darkMode === "enabled") {
+  // 페이지 로드될 떄마다 체크, 저장
+  enableDarkMode();
+}
+darkModeBtn.addEventListener("click", () => {
+  darkMode = localStorage.getItem(DARK_MODE); // 그때그때 마다 저장
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
